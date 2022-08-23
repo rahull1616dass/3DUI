6b66d4e349d4a3ce26908a95400397a3f4e9ac37
Rahul <rahull1616dass@gmail.com>
2022-08-23 15:56:54 +0200

    LOL

---
+++ Assets/3DUI/scripts/ObjectCreator.cs
@@ -0,0 +1,34 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ObjectCreator : MonoBehaviour
+{
+    [SerializeField] private GameObject BlockPrefab;
+    [SerializeField] private GameObject CylinderPrefab;
+    private List<GameObject> BlocksCreated = new List<GameObject>();
+
+    public void CreateBlockPrefab(Transform Where)
+    {
+        GameObject blockInstance = Instantiate(BlockPrefab, Where.position, Quaternion.identity);
+
+        blockInstance.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        BlocksCreated.Add(blockInstance);
+    }
+
+    public void CreateCylinderPrefab(Transform Where)
+    {
+        GameObject blockInstance = Instantiate(CylinderPrefab, Where.position, Quaternion.identity);
+
+        blockInstance.transform.localScale = new Vector3(1.0f, 1.0f, 1.0f);
+        BlocksCreated.Add(blockInstance);
+    }
+
+    public void DestroyAllCreatedBlocks()
+    {
+        foreach (var b in BlocksCreated)
+        {
+            Destroy(b);
+        }
+    }
+}
